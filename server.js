3c1300f19d53c93e3346823b28b498263c8142ac
Mihajlo <mihajlosupic@gmail.com>
2019-01-28 18:21:16 +0100

    Update server with good port

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('hello, i know nodejitsu\n'); }).listen(8080);
+const PORT = process.env.PORT || 8000
+
+const http = require('http');
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Mihajlos test page');
+}).listen(PORT);
