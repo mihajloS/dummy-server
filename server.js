d159c56494e3954b0abe58a6fbd5dc6b0e64a037
Mihajlo <mihajlosupic@gmail.com>
2019-02-02 00:27:47 +0100

    Add crossdomain support. Add initial unit test

--- server.js
+++ server.js
@@ -4,12 +4,31 @@ const app = express()
 const PORT = process.env.PORT || 8000
 const contact = require('./contact/contact')
 const mongoose = require('mongoose')
-const config = require('config')
+const config = require('config').db
 
-mongoose.connect(config.db.dbUri, { useNewUrlParser: true })
+mongoose.connect(config.dbUri, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'Db error:'))
-db.on('open', console.log.bind(console, 'Db connection success'))
+db.on('open', ()=>{
+    console.log(`Db connection success [${config.description}]`)
+    app.emit('appStarted')
+})
+
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 
 app.use(express.json()) // for parsing application/json
 app.use(morgan('dev')) // for loging requests
@@ -19,3 +38,5 @@ app.get('/', (req, res) => { res.send('Index page') })
 app.route('/contact_mihajlo').post(contact.emailToMihajlo)
 
 app.listen(PORT, () => { console.log(`App listening on port ${PORT}`) })
+
+module.exports = app // for testing
---
+++ test/email.js
@@ -0,0 +1,32 @@
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../server')
+const should = chai.should()
+chai.use(chaiHttp)
+
+describe('Email', () =>{
+    describe('SEND email', () => {
+        it('it should use \"contact_mihajlo\" api correctly', (done)=>{
+            const newEmail = {from: "mail@mail.com", message: "Hey you"}
+            console.log('ejj')
+            chai.request(server)
+            .post('/contact_mihajlo')
+            .send(newEmail)
+            .end((err, res) => {
+                if (err) done(err)
+                checkEmailPositiveRes(newEmail, res)
+                done()
+            })
+        })
+    })
+})
+
+function checkEmailPositiveRes(sent, res) {
+    res.should.have.status(200)
+    res.body.should.have.property('_id')
+    res.body.should.have.property('from').eql(sent.from)
+    res.body.should.have.property('message').eql(sent.message)
+    res.body.should.have.property('createdAt')
+}
