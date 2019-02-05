2fe9629826871d85a0a0b7a25688a5de42a23ff0
Mihajlo <mihajlosupic@gmail.com>
2019-02-05 10:30:19 +0100

    Update access origin

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ db.on('open', ()=>{
 })
 
 const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '37.48.77.137');
+    res.header('Access-Control-Allow-Origin', 'mihajlosupic.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
