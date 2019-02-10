51d923f0147009a35de36a7443a732a8163071f6
Mihajlo <mihajlosupic@gmail.com>
2019-02-10 12:48:46 +0100

    FIx spaces

--- contact/contact.js
+++ contact/contact.js
@@ -3,24 +3,24 @@ const Email = require('../dbmodels/contact/email')
 const config = require("config").email
 
 function emailToMihajlo(req, res) {
-    console.log('email to mihajlo', req.body)
-    const message = req.body.message
+  console.log('email to mihajlo', req.body)
+  const message = req.body.message
 	const from = req.body.from
-    let promise = main(from, message).catch(e => console.log("main email error", e))
+	let promise = main(from, message).catch(e => console.log("main email error", e))
 
-    promise.then((success)=>{
-		if (!success) return res(success)
-		const new_email = new Email({from, message})
-		new_email.save((err, email )=> {
-			if (err) {
-				console.error('Email error', err)
-				res.send(err)
-			}
-			else {
-				console.log('email sent', email)
-				res.json(email)
-			}
-		})
+    promise.then((success) => {
+			if (!success) return res(success)
+			const new_email = new Email({from, message})
+			new_email.save((err, email )=> {
+				if (err) {
+					console.error('Email error', err)
+					res.send(err)
+				}
+				else {
+					console.log('email sent', email)
+					res.json(email)
+				}
+			})
     })
 }
 
