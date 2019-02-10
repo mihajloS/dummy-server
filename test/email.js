70ad05259e69e39b15c1920288d02f3d7c61e0fa
Mihajlo <mihajlosupic@gmail.com>
2019-02-10 23:19:47 +0100

    Lint unti test

--- test/email.js
+++ test/email.js
@@ -1,32 +1,37 @@
-process.env.NODE_ENV = 'test'
+process.env.NODE_ENV = 'test';
 
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const server = require('../server')
-const should = chai.should()
-chai.use(chaiHttp)
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+chai.use(chaiHttp);
 
+/** Email testss */
 describe('Email', () =>{
-    describe('SEND email', () => {
-        it('it should use \"contact_mihajlo\" api correctly', (done)=>{
-            const newEmail = {from: "mail@mail.com", message: "Hey you"}
-            console.log('ejj')
-            chai.request(server)
-            .post('/contact_mihajlo')
-            .send(newEmail)
-            .end((err, res) => {
-                if (err) done(err)
-                checkEmailPositiveRes(newEmail, res)
-                done()
-            })
-        })
-    })
-})
+  describe('SEND email', () => {
+    it('it should use \"contact_mihajlo\" api correctly', (done)=>{
+      const newEmail = {from: 'mail@mail.com', message: 'Hey you'};
+      console.log('ejj');
+      chai.request(server)
+          .post('/contact_mihajlo')
+          .send(newEmail)
+          .end((err, res) => {
+            if (err) done(err);
+            checkEmailPositiveRes(newEmail, res);
+            done();
+          });
+    });
+  });
+});
 
+/**
+ * Compare sent and received email
+ * @param {Object} sent
+ * @param {Object} res
+ */
 function checkEmailPositiveRes(sent, res) {
-    res.should.have.status(200)
-    res.body.should.have.property('_id')
-    res.body.should.have.property('from').eql(sent.from)
-    res.body.should.have.property('message').eql(sent.message)
-    res.body.should.have.property('createdAt')
+  res.should.have.status(200);
+  res.body.should.have.property('_id');
+  res.body.should.have.property('from').eql(sent.from);
+  res.body.should.have.property('message').eql(sent.message);
+  res.body.should.have.property('createdAt');
 }
